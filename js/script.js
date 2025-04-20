b8bc5146215d2c7c4b4b00ba4fa8e9410f65056a
Cathleen Vu <cathleendvu@gmail.com>
2025-04-19 20:48:54 -0700

    Update email form to be responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+// Animation
 AOS.init({
     duration: 1000,
     once: true
+});
+
+// Clear form data after submission
+const form = document.getElementById("right-connect");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  fetch(form.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(() => {
+    form.reset();
+  });
 });
